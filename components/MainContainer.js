3294e2c6993612e6285dada7c11fc536a32c67ab
Luka Kilaberidze <lkila16@freeuni.edu.ge>
2022-08-10 22:44:00 +0400

    fix white container sizes on desktop & mobile

--- components/MainContainer.js
+++ components/MainContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function MainContainer({ children }) {
   return (
-    <div className="w-full sm:w-[512px] px-5 py-10 sm:p-15 bg-white rounded-md shadow-md">
+    <div className="w-full sm:w-max px-5 py-10 sm:px-10 sm:py-20 bg-white rounded-md shadow-md">
       {children}
     </div>
   );
